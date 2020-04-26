78ff7ec516028e676289f8f511a9cbc7d908a8f9
wy19960827 <44884878+wy19960827@users.noreply.github.com>
2020-04-26 10:26:39 +0800

    爬取虎扑论坛信息
    爬取标题、作者、发布时间等信息，设置代理ip反反爬，使用for循环来循环页面链接，使用xpath提取页面内容，由于replyandscan元素中包含'\xa0',会出现报错，所以需要单独处理，将浏览情况与回复情况拆分。爬取到的数据通过items.py存储至MySQL。

---
+++ crawltosql/crawltosql/pipelines.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import pymysql
+
+
+class CrawltosqlPipeline(object):
+    def __init__(self):
+        self.conn = pymysql.connect(host="localhost", user="root", passwd="ddd123", db="mydata")
+
+    def process_item(self, item, spider):
+        mylen = min(len(item["url"]), len(item["title"]), len(item["author"]), len(item["reply"]), len(item["scan"]),
+                    len(item["pubtime"]), len(item["lastreplytime"]), len(item["endauthor"]))
+        for j in range(mylen):
+            # print(j)
+            url = "https://bbs.hupu.com" + item["url"][j]
+            title = item["title"][j]
+            author = item["author"][j]
+            authorlink = item["authorlink"][j]
+            reply = item["reply"][j]
+            scan = item["scan"][j]
+            pubtime = item["pubtime"][j]
+            lastreplytime = item["lastreplytime"][j]
+            endauthor = item["endauthor"][j]
+            # 构造对应的sql语句，实现获取列的对应的数据插入到数据库中
+            sql = "insert into data(url,title,author,authorlink,reply,scan,pubtime,lastreplytime,endauthor) VALUES('" + url + "','" + title + "','" + author + "','" + authorlink + "','" + reply + "','" + scan + "','" + pubtime + "','" + lastreplytime + "','" + endauthor + "')"
+            # 通过query实现执行对应的sql语句
+            self.conn.query(sql)
+            self.conn.commit()
+
+        return item
+
+    def close_spider(self, spider):
+        self.conn.close()
